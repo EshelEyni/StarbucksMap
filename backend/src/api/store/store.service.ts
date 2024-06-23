73789e364e3f20acdb99147fe0d436be3acb61d0
Eshel Eyni <esheleyni@gmail.com>
2024-06-24 00:12:28 +0300

    feat(BE): Added store filtering by country boundary

--- backend/src/api/store/store.service.ts
+++ backend/src/api/store/store.service.ts
@@ -2,6 +2,13 @@ import axios from "axios";
 import { getName } from "country-list";
 import { StoreData } from "../../../../shared/types/system";
 import { point, polygon, booleanPointInPolygon } from "@turf/turf";
+import { Feature, GeoJsonProperties, Polygon, Position } from "geojson";
+import { logger } from "../../services/logger.service";
+
+type OpenStreetMapResponse = {
+  type: string;
+  coordinates: Position[][][];
+} | null;
 
 const url = "https://raw.githubusercontent.com/mmcloughlin/starbucks/master/locations.json";
 
@@ -10,15 +17,15 @@ async function query({ country }: { country: string }) {
   let stores = response.data as StoreData[];
   const countries = getListOfCountries(stores);
 
-  if (country !== "all") {
-    const countryBoundary = await fetchCountryBoundary(country); // Function to get country boundary as a GeoJSON polygon
-    const countryPolygon = polygon(countryBoundary.coordinates);
-
-    stores = stores.filter(store => {
-      const storePoint = point([store.longitude, store.latitude]);
-      return booleanPointInPolygon(storePoint, countryPolygon);
-    });
+  if (country === "all") {
+    return {
+      countries,
+      stores,
+    };
   }
+
+  const countryBoundary = await fetchCountryBoundary(country);
+  stores = filterByCountry({ stores, country, countryBoundary });
 
   return {
     countries,
@@ -26,11 +33,42 @@ async function query({ country }: { country: string }) {
   };
 }
 
-async function fetchCountryBoundary(country: string) {
-  const response = await axios.get(
-    `https://nominatim.openstreetmap.org/search?format=json&q=${country}&polygon_geojson=1&limit=1`
+function filterByCountry({
+  stores,
+  country,
+  countryBoundary,
+}: {
+  stores: StoreData[];
+  country: string;
+  countryBoundary: OpenStreetMapResponse;
+}) {
+  // If no country boundary is found, filter by country code
+  if (!countryBoundary) {
+    return stores.filter(store => store.country === country);
+  }
+
+  const countryPolygons = countryBoundary.coordinates.map((coords: Position[][]) =>
+    polygon(coords)
   );
-  return response.data[0].geojson.coordinates;
+
+  return stores.filter(store => {
+    const storePoint = point([store.longitude, store.latitude]);
+    return countryPolygons.some((poly: Feature<Polygon, GeoJsonProperties>) =>
+      booleanPointInPolygon(storePoint, poly)
+    );
+  });
+}
+
+async function fetchCountryBoundary(country: string): Promise<OpenStreetMapResponse> {
+  try {
+    const response = await axios.get(
+      `https://nominatim.openstreetmap.org/search?format=json&q=${country}&polygon_geojson=1&limit=1`
+    );
+    return response.data[0].geojson;
+  } catch (error) {
+    logger.error("Error fetching country boundary", error);
+    return null;
+  }
 }
 
 function getListOfCountries(data: StoreData[]) {
